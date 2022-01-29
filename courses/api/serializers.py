d4ff67bdefc4563ec8763f1d991dcd556815ed8b
Cell <suhail9453531504@gmail.com>
2022-01-29 22:19:09 +0530

    Added Serializers

--- courses/api/serializers.py
+++ courses/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from ..models import Subject
+from ..models import Subject, Course, Module
 
 # from courses.models import Subject
 # from courses.api.serializers import SubjectSerializer
@@ -7,7 +7,24 @@ from ..models import Subject
 # serializer = SubjectSerializer(subject)
 # serializer.data
 
+
 class SubjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subject
         fields = ['id', 'title', 'slug']
+
+
+class ModuleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Module
+        fields = ['order', 'title', 'description']
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    # many=True to indicate that we're serializing multiple obj. read_only to indicate this field is read only
+    modules = ModuleSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Course
+        fields = ['id', 'subject', 'title', 'slug',
+                  'overview', 'created', 'owner', 'modules']
