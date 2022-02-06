d9fdb2ce4e19bb75e0f4ad0faa5805a764b356e2
Cell <suhail9453531504@gmail.com>
2022-02-06 22:41:25 +0530

    Created a chat server using Channels

--- educa/settings.py
+++ educa/settings.py
@@ -152,12 +152,16 @@ LOGIN_REDIRECT_URL = reverse_lazy('student_course_list')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
+
+#
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
         'LOCATION': '127.0.0.1:11211',
     }
 }
+
+# REDIS: Open Ubuntu shell; cd /etc/redis && sudo service redis-server start
 # Defines the configuration for the channel layers available to project.
 CHANNEL_LAYERS = {
     'default': {
